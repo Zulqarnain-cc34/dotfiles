440b4d1e69fc8b3469a3019fa1f13b01ab4850fd
Zulqarnain-cc34 <alphatech345@protonmail.com>
2021-05-18 18:45:23 +0500

    Nvim compe setup

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -1,11 +1,12 @@
 local g = vim.g
 
-local map = function(type, key, value, set)
-    set = set or 'g' --Default global mapping
-    if set == 'g' then
-	vim.api.nvim_set_keymap(type, key, value, {noremap = true, silent = true});
-    elseif set == 'b' then
-	vim.api.nvim_buf_set_keymap(0, type, key, value, {noremap = true, silent = true});
+local map = function(type, key, value, mode, opts)
+    mode = mode or 'g' -- Default global mapping
+    opts = opts or {noremap = true, silent = true}
+    if mode == 'g' then
+        vim.api.nvim_set_keymap(type, key, value, opts);
+    elseif mode == 'b' then
+        vim.api.nvim_buf_set_keymap(0, type, key, value, opts);
     end
 end
 
@@ -28,34 +29,32 @@ map('n', '<leader>lR', ':LspRestart<cr>')
 -- ===================== Lsp KeyBindings ==================================
 
 --  Highlights and Goto Declarations of function
-map('n', '<leader>lD', '<cmd>lua vim.lsp.buf.declaration()<CR>','b')
+map('n', '<leader>lD', '<cmd>lua vim.lsp.buf.declaration()<CR>', 'b')
 -- Goto Definations of functions
-map('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<CR>','b')
+map('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<CR>', 'b')
 -- Show function doc on hover
-map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>','b')
+map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', 'b')
 -- Show and goto refrences of functions
-map('n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<CR>','b')
+map('n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<CR>', 'b')
 -- Shows the function signature
-map('n', '<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<CR>','b')
+map('n', '<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<CR>', 'b')
 -- Shows function implementation
-map('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<CR>','b')
+map('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<CR>', 'b')
 -- Goto type defination
-map('n', '<leader>ly', '<cmd>lua vim.lsp.buf.type_definition()<CR>','b')
+map('n', '<leader>ly', '<cmd>lua vim.lsp.buf.type_definition()<CR>', 'b')
 -- Format Code according to file specific formatter
-map('n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>','b')
+map('n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', 'b')
 -- Shows the variable and function symbols
-map('n', '<leader>ds', '<cmd>lua vim.lsp.buf.document_symbol()<CR>','b')
+map('n', '<leader>ds', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', 'b')
 -- Shows thw workspace Symbols
-map('n', '<leader>ws', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>','b')
+map('n', '<leader>ws', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', 'b')
 -- Make a code action based on diagnostics
-map('n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<CR>','b')
+map('n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<CR>', 'b')
 -- Shows the Diagnostics of a line
-map('n', '<leader>ll', '<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>','b')
+map('n', '<leader>ll', '<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>', 'b')
 -- Bulk rename
-map('n', '<leader>le', '<cmd>lua vim.lsp.buf.rename()<CR>','b')
+map('n', '<leader>le', '<cmd>lua vim.lsp.buf.rename()<CR>', 'b')
 -- Shows Incoming
-map('n', '<leader>lc', '<cmd>lua vim.lsp.buf.incoming_calls()<CR>','b')
+map('n', '<leader>lc', '<cmd>lua vim.lsp.buf.incoming_calls()<CR>', 'b')
 -- Shows Outgoing
-map('n', '<leader>lo', '<cmd>lua vim.lsp.buf.outgoing_calls()<CR>','b')
-
-
+map('n', '<leader>lo', '<cmd>lua vim.lsp.buf.outgoing_calls()<CR>', 'b')
--- nvim/lua/lsp/python-ls.lua
+++ nvim/lua/lsp/python-ls.lua
@@ -1,1 +1,13 @@
-require'lspconfig'.pyright.setup{}
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    'documentation',
+    'detail',
+    'additionalTextEdits',
+  }
+}
+
+require'lspconfig'.pyright.setup{
+        capabilities=capabilities
+}
