5306df4ba498302c2a2dbb4e3ef4a7bf98396730
Zulqarnain-cc34 <alphatech345@protonmail.com>
2021-12-06 21:07:57 +0500

    Fixed mpv polybar long error problem

--- polybar/scripts/polybar-mpv/mpv.sh
+++ polybar/scripts/polybar-mpv/mpv.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
-if [ "$1" == "--status" ]; then
-echo '{ "command": ["get_property", "core-idle"] }' | socat - /tmp/mpv_socket | jq .data
 
-elif [ "$1" == "--title" ]; then
-echo '{ "command": ["get_property", "media-title"] }' | socat - /tmp/mpv_socket | jq .data | tr '"' ' ' | sed -e 's/\.[^.]*$//'
+if [ "$(pidof mpv)" ]; then
+    if [ "$1" = "--status" ]; then
+    echo '{ "command": ["get_property", "core-idle"] }' | socat - /tmp/mpv_socket | jq .data
+    
+    elif [ "$1" = "--title" ]; then
+    echo '{ "command": ["get_property", "media-title"] }' | socat - /tmp/mpv_socket | jq .data | tr '"' ' ' | sed -e 's/\.[^.]*$//'
+    
+    elif [ "$1" = "--volume-up" ]; then
+    tmp_vol=$(echo '{ "command": ["get_property", "volume"] }' | socat - /tmp/mpv_socket | jq .data | cut -d "." -f 1 )
+    tmp_vol=$((tmp_vol+5))
+    echo '{ "command": ["set_property", "volume", '$tmp_vol'] }' | socat - /tmp/mpv_socket
+    
+    elif [ "$1" = "--volume-down" ]; then
+    tmp_vol=$(echo '{ "command": ["get_property", "volume"] }' | socat - /tmp/mpv_socket | jq .data | cut -d "." -f 1 )
+    tmp_vol=$((tmp_vol-5))
+    echo '{ "command": ["set_property", "volume", '$tmp_vol'] }' | socat - /tmp/mpv_socket
+    fi
 
-elif [ "$1" == "--volume-up" ]; then
-tmp_vol=$(echo '{ "command": ["get_property", "volume"] }' | socat - /tmp/mpv_socket | jq .data | cut -d "." -f 1 )
-tmp_vol=$((tmp_vol+5))
-echo '{ "command": ["set_property", "volume", '$tmp_vol'] }' | socat - /tmp/mpv_socket
-
-elif [ "$1" == "--volume-down" ]; then
-tmp_vol=$(echo '{ "command": ["get_property", "volume"] }' | socat - /tmp/mpv_socket | jq .data | cut -d "." -f 1 )
-tmp_vol=$((tmp_vol-5))
-echo '{ "command": ["set_property", "volume", '$tmp_vol'] }' | socat - /tmp/mpv_socket
-
-fi
-
+    else
+        exit
+fi 
