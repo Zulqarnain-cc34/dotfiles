3a3353a87e709582a55a6343b60a87c64b737d35
Zulqarnain-cc34 <alphatech345@protonmail.com>
2025-12-23 16:54:38 +0500

    removed nvim-cmp major Change blink is added

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,5 +1,3 @@
--- lua/plugins.lua (Converted to lazy.nvim)
-
 -- ===================================
 -- _______  _             _
 -- |  _ \| |_   _  __ _(_)_ __  ___
@@ -9,23 +7,20 @@
 --               |___/
 -- ===================================
 
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- OLD LINE: if not vim.loop.fs_stat(lazypath) then
-if vim.fn.isdirectory(lazypath) == 0 then -- Check if the directory does NOT exist
+if vim.fn.isdirectory(lazypath) == 0 then
     vim.fn.system({
         "git",
         "clone",
         "--filter=blob:none",
         "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
+        "--branch=stable",
         lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
 
 return require('lazy').setup({
-    -- Configuration for nvim-lspconfig
     {
         'neovim/nvim-lspconfig',
         config = function()
@@ -45,7 +40,7 @@ return require('lazy').setup({
     },
     {
         "folke/persistence.nvim",
-        event = "BufReadPre", -- this will only start session saving when an actual file was opened
+        event = "BufReadPre",
     },
     {
         "folke/styler.nvim",
@@ -65,10 +60,7 @@ return require('lazy').setup({
     {
         'stevearc/oil.nvim',
         opts = {},
-        -- Optional dependencies
         dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-        -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-        -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
         lazy = false,
     },
     {
@@ -88,34 +80,30 @@ return require('lazy').setup({
         "NeogitOrg/neogit",
         lazy = true,
         dependencies = {
-            "nvim-lua/plenary.nvim",  -- required
-            "sindrets/diffview.nvim", -- optional - Diff integration
+            "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
 
-            -- Only one of these is needed.
-            "ibhagwan/fzf-lua",    -- optional
-            "nvim-mini/mini.pick", -- optional
-            "folke/snacks.nvim",   -- optional
+            "ibhagwan/fzf-lua",
+            "nvim-mini/mini.pick",
+            "folke/snacks.nvim",
         },
         cmd = "Neogit",
         keys = {
             { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
         }
     },
-    -- Icons (Explicitly listed as it has configuration in lua/plugins/nvim-web-devicons.lua)
     {
         "kyazdani42/nvim-web-devicons",
         config = function()
             require("plugins.nvim-web-devicons")
         end
     },
-
     {
         'kevinhwang91/nvim-hlslens',
         config = function()
             require("plugins.hlslens")
         end
     },
-
     {
         "folke/tokyonight.nvim",
         lazy = false,
@@ -128,8 +116,6 @@ return require('lazy').setup({
             require("plugins.diaglist")
         end
     },
-
-    -- opt = true in packer usually means the plugin is lazy-loaded by default
     { "Tastyep/structlog.nvim" },
 
     -- ================== LSP, Autocomplete and Treesitter =====================
@@ -142,50 +128,13 @@ return require('lazy').setup({
     },
     {
         "navarasu/onedark.nvim",
-        priority = 1000, -- make sure to load this before all the other start plugins
+        priority = 1000,
         config = function()
             require('onedark').setup {
                 style = 'darker'
             }
             require('onedark').load()
         end
-    },
-    {
-
-        'hrsh7th/nvim-cmp',
-        event = 'InsertEnter',
-        dependencies = {
-            -- Snippet Engine & its associated nvim-cmp source
-            {
-                'L3MON4D3/LuaSnip',
-                build = (function()
-                    -- Build Step is needed for regex support in snippets.
-                    -- This step is not supported in many windows environments.
-                    -- Remove the below condition to re-enable on windows.
-                    if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-                        return
-                    end
-                    return 'make install_jsregexp'
-                end)(),
-                dependencies = {
-                    -- `friendly-snippets` contains a variety of premade snippets.
-                    --    See the README about individual language/framework/plugin snippets:
-                    --    https://github.com/rafamadriz/friendly-snippets
-                    {
-                        'rafamadriz/friendly-snippets',
-                        config = function()
-                            require('luasnip.loaders.from_vscode').lazy_load()
-                        end,
-                    },
-                },
-            },
-            'saadparwaiz1/cmp_luasnip',
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-path',
-        },
-        config = function()
-            require('plugins.nvim-cmp')
-        end,
     },
     {
         "folke/which-key.nvim",
@@ -225,9 +174,6 @@ return require('lazy').setup({
         priority = 1000,
         lazy = false,
         opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
             bigfile = { enabled = true },
             dashboard = { enabled = true },
             explorer = { enabled = true },
@@ -246,15 +192,11 @@ return require('lazy').setup({
         'windwp/nvim-autopairs',
         event = "InsertEnter",
         config = true
-        -- use opts = {} for passing setup options
-        -- this is equivalent to setup({}) function
     },
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        lazy = false, -- Keep this to load it eagerly
-        -- We remove the custom config function and use 'main' to load the module
-        -- This ensures the module is added to the runtime path before configuration begins.
+        lazy = false,
         opts = {
             ensure_installed = {
                 "lua", "python", "bash", "go", "html", "css", "javascript", "json", "c", "cpp", "markdown",
@@ -268,11 +210,10 @@ return require('lazy').setup({
             indent = {
                 enable = false,
             },
-            -- Consolidated rainbow settings (from the old nvim-ts-rainbow.lua):
             rainbow = {
                 enable = true,
-                extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-                max_file_lines = 1000 -- Use the highest value from your original configs
+                extended_mode = true,
+                max_file_lines = 1000
             },
         },
     },
@@ -298,13 +239,32 @@ return require('lazy').setup({
     -- Lsp - Ui
     {
         'nvimdev/lspsaga.nvim',
-        -- Since `after = 'nvim-lspconfig'` exists, we implicitly rely on it loading first
         dependencies = { 'neovim/nvim-lspconfig' },
         config = function()
             require('lspsaga').setup({})
         end,
     },
+    {
+        'saghen/blink.cmp',
+        dependencies = { 'rafamadriz/friendly-snippets' },
+        opts = {
+            keymap = {
+                preset = 'default',
+                ['<Tab>'] = { 'snippet_forward', 'fallback' },
+                ['<S-Tab>'] = { 'snippet_backward', 'fallback' }
+            },
+            appearance = {
+                nerd_font_variant = 'mono'
+            },
+            completion = { documentation = { auto_show = false } },
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
 
+            fuzzy = { implementation = "prefer_rust_with_warning" }
+        },
+        opts_extend = { "sources.default" }
+    },
     {
         'folke/trouble.nvim',
         dependencies = 'nvim-web-devicons',
