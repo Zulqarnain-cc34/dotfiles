de12feb7082452129f91293cdd4f163c4ffe028a
Zulqarnain-cc34 <alphatech345@protonmail.com>
2025-12-23 15:42:07 +0500

    some plugins useless now removed and their configs

--- nvim/lua/lsp/C.lua
+++ nvim/lua/lsp/C.lua
@@ -1,12 +1,4 @@
-local lsp_status = require('lsp-status')
-lsp_status.register_progress()
-
-vim.lsp.config['clangd'] = {
-    -- capabilities = lsp_status.capabilities,
-   on_attach = lsp_status.on_attach,
-    init_options = {clangdFileStatus = true},
-    handlers = lsp_status.extensions.clangd.setup()
-}
+vim.lsp.config['clangd'] = {}
 
 
 vim.lsp.enable('clangd')
