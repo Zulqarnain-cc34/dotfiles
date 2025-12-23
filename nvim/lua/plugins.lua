1a646092996cdb3f71721f842bd87f3c3367cd8c
Zulqarnain-cc34 <alphatech345@protonmail.com>
2025-12-23 12:55:09 +0500

    added one-dark nvim theme and chagned README.md with emojis

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -190,6 +190,16 @@ return require('lazy').setup({
         end
     },
     {
+        "navarasu/onedark.nvim",
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require('onedark').setup {
+                style = 'darker'
+            }
+            require('onedark').load()
+        end
+    },
+    {
 
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
