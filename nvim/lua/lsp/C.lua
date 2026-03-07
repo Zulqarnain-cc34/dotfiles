eb7edd893ff053943f33c43ed5729b80d0e075ba
Zulqarnain-cc34 <alphatech345@protonmail.com>
2026-03-08 00:00:18 +0500

    nvim clang d config

--- nvim/lua/lsp/C.lua
+++ nvim/lua/lsp/C.lua
@@ -1,4 +1,18 @@
-vim.lsp.config['clangd'] = {}
-
+vim.lsp.config['clangd'] = {
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--completion-style=detailed",
+    "--header-insertion=never",
+    "--offset-encoding=utf-8",
+  },
+  root_dir = vim.lsp.util.root_pattern(
+    "compile_commands.json",
+    "CMakeLists.txt",
+    ".git"
+  ),
+  filetypes = { "c", "cpp", "objc", "objcpp" },
+}
 
 vim.lsp.enable('clangd')
