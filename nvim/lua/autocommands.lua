93de136f65ad284d8d957b0a04c795d6b5fe9edb
Zulqarnain-cc34 <alphatech345@protonmail.com>
2026-04-01 13:00:54 +0500

    main

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -120,4 +120,26 @@ vim.api.nvim_create_autocmd('BufEnter', {
 -- Description: Set custom color for comments
 
 -- Set comment color (adjust to your preference)
-vim.api.nvim_set_hl(0, 'Comment', { fg = '#968d8c' })
+-- vim.api.nvim_set_hl(0, 'Comment', { fg = '#968d8c' })
+
+local lazy_cmds = vim.api.nvim_create_augroup('lazy_cmds', { clear = true })
+local snapshot_dir = vim.fn.stdpath('data') .. '/plugin-snapshot'
+local lockfile = vim.fn.stdpath('config') .. '/lazy-lock.json'
+
+vim.api.nvim_create_user_command(
+    'BrowseSnapshots',
+    'edit ' .. snapshot_dir,
+    {}
+)
+
+vim.api.nvim_create_autocmd('User', {
+    group = lazy_cmds,
+    pattern = 'LazyUpdatePre',
+    desc = 'Backup lazy.nvim lockfile',
+    callback = function(event)
+        vim.fn.mkdir(snapshot_dir, 'p')
+        local snapshot = snapshot_dir .. os.date('/%Y-%m-%dT%H:%M:%S.json')
+
+        vim.uv.fs_copyfile(lockfile, snapshot)
+    end,
+})
